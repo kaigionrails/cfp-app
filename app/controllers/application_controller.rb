9bb041743575f2176ed9aba443a0eae2295e6425
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-06-30 02:43:45 +0900

    Strip referrer host on redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
     elsif !user.complete?
       edit_profile_path
     elsif request.referrer.present? && request.referrer != new_user_session_url
-      request.referrer
+      URI.parse(request.referrer).path
     elsif session[:target]
       session.delete(:target)
     elsif user.staff_for?(current_event)
