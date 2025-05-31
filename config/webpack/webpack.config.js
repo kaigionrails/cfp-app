45ef3404daefffc376242b30ae3a54140bfcfb41
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-05-31 21:01:25 +0900

    Upgrade shakapacker npm package to v7

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,4 +1,4 @@
-const { webpackConfig, merge } = require('shakapacker')
+const { generateWebpackConfig, merge } = require('shakapacker')
 
 const customConfig = {
   resolve: {
@@ -20,4 +20,4 @@ const customConfig = {
   }
 }
 
-module.exports = merge(webpackConfig, customConfig)
+module.exports = merge(generateWebpackConfig(), customConfig)
